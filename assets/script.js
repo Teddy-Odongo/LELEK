c035bf4dd3e576da217a6d87b8ac71c0029b2c7e
Kings Tedd <teddyodongo22@gmail.com>
2025-02-24 16:26:24 -0800

    Color changes

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() <= 40){
-            $('.navbar').removeClass('scroll_navbar')
-        }else{
-            $('.navbar').Class('scroll_navbar')
-        }
-    })
-})
